94d6fe96bd5b7297422bba06df76ffc49d299493
Andrew Zerr <azerr@qualitylogic.com>
2023-04-22 12:18:38 -0600

    Update color variables used

--- src/pages/blog-page.tsx
+++ src/pages/blog-page.tsx
@@ -4,7 +4,7 @@ import { colors } from '../colors';
 const BlogPage = () => {
   return (
     <div id='blog-page'>
-      <Typography component="body" sx={{color: colors.mainText}}>
+      <Typography component="body" sx={{color: colors.white500}}>
         Blog where all the cool blog stuff goes.
       </Typography>
     </div>
